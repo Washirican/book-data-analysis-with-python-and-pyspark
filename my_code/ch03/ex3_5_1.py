0411e5781be1c77bfcdf36e6c382403d86c85e21
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2026-02-10 12:50:26 -0800

    Add chapter 3 exercise files.

---
+++ my_code/ch03/ex3_5_1.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+spark = (
+    SparkSession.builder.appName("Analyzing the vocabulary of Pride and Prejudice.")
+    .config("spark.sql.repl.eagerEval.enabled", "False")
+    .getOrCreate()
+)
+
+spark.sparkContext.setLogLevel("OFF")
+
+# Transformations
+results = (
+    spark.read.text("./data/gutenberg_books/1342-0.txt")
+    .select(F.split(F.col("value"), " ").alias("line"))
+    .select(F.explode(F.col("line")).alias("word"))
+    .select(F.lower(F.col("word")).alias("word"))
+    .select(F.regexp_extract(F.col("word"), "[a-z]+", 0).alias("word"))
+    .filter(F.col("word") != "")
+#    .groupby("word")
+    .select(F.substring(F.col("word"), 1, 1).alias("first_letter"))
+    .groupBy(F.col("first_letter"))
+    .count()
+    .orderBy(F.col("count"), ascending=False)
+)
+
+# Actions
+results.show(5)
