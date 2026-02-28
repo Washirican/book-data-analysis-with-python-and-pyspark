e6f7a92b8502785b1e32754a57e4055e3cc056e5
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2026-02-28 12:00:39 -0800

    Update chapter exercises files.

--- my_code/ch05/broadcast_logs.py
+++ my_code/ch05/broadcast_logs.py
@@ -7,6 +7,7 @@ import pyspark.sql.functions as F
 spark = SparkSession.builder.getOrCreate()
 
 DIRECTORY = "./data/broadcast_logs"
+
 logs = (
     spark.read.csv(
         os.path.join(DIRECTORY, "BroadcastLogs_2018_Q3_M8.CSV"),
@@ -24,4 +25,39 @@ logs = (
             + F.col("Duration").substr(7, 2).cast("int")
         ),
     )
+)
+
+log_identifier = spark.read.csv(
+    os.path.join(DIRECTORY, "ReferenceTables/LogIdentifier.csv"),
+    sep="|",
+    header=True,
+    inferSchema=True,
+)
+
+logs_and_channels = logs.join(log_identifier, on="LogServiceID", how="inner")
+
+cd_category = spark.read.csv(
+    os.path.join(DIRECTORY, "ReferenceTables/CD_Category.csv"),
+    sep="|",
+    header=True,
+    inferSchema=True,
+).select(
+    "CategoryID",
+    "CategoryCD",
+    F.col("EnglishDescription").alias("Category_Description"),
+)
+
+cd_program_class = spark.read.csv(
+    os.path.join(DIRECTORY, "ReferenceTables/CD_ProgramClass.csv"),
+    sep="|",
+    header=True,
+    inferSchema=True,
+).select(
+    "ProgramClassID",
+    "ProgramClassCD",
+    F.col("EnglishDescription").alias("ProgramClass_Description"),
+)
+
+full_log = logs_and_channels.join(cd_category, "CategoryID", how="left").join(
+    cd_program_class, "ProgramClassID", how="left"
 )
